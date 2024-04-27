ea5dfd4d4dffb6e4f4a0c1e5b1e12fd68cd3edf5
EREN SAVASIR <erensavasir47@gmail.com>
2024-04-27 22:12:28 +0300

    yonetici islemleri

--- vizegorselsinavi22/Controllers/idare1Controller.cs
+++ vizegorselsinavi22/Controllers/idare1Controller.cs
@@ -13,9 +13,44 @@ namespace vizegorselsinavi22.Controllers
         // GET: idare1
         public ActionResult Index()
         {
-            vizegorselsinavEntities1 db = new vizegorselsinavEntities1();
+            
             var degerler = db.tbl_idari.ToList();
             return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult Yeniyonetici()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Yeniyonetici(tbl_idari p1)
+        {
+            db.tbl_idari.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult Sil(int id)
+        {
+            var idari = db.tbl_idari.Find(id);
+            db.tbl_idari.Remove(idari);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult yoneticiguncelle(int id)
+        {
+            var yonetici = db.tbl_idari.Find(id);
+            return View("yoneticiguncelle", yonetici);
+        }
+        public ActionResult Guncelle(tbl_idari p1)
+        {
+            var idare = db.tbl_idari.Find(p1.idariid);
+            idare.idariAdSoyad = p1.idariAdSoyad;
+            idare.idariUnvan = p1.idariUnvan;
+            idare.idariBasla = p1.idariBasla;
+            idare.idariCikis = p1.idariCikis;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
         }
     }
 }
