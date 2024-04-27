3c9b9022d8dbaf156874822284e348fb07e18d67
EREN SAVASIR <erensavasir47@gmail.com>
2024-04-27 21:37:57 +0300

    ogretmen sil/guncelle/ekle bitti

--- vizegorselsinavi22/Controllers/ogretmen1Controller.cs
+++ vizegorselsinavi22/Controllers/ogretmen1Controller.cs
@@ -29,5 +29,26 @@ namespace vizegorselsinavi22.Controllers
             db.SaveChanges();
             return View();
         }
+        public ActionResult Sil(int id)
+        {
+            var ogretmen = db.tbl_ogretmen.Find(id);
+            db.tbl_ogretmen.Remove(ogretmen);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult ogretmenguncelle(int id)
+        {
+            var ogrtgun = db.tbl_ogretmen.Find(id);
+            return View("ogretmenguncelle", ogrtgun);
+        }
+        public ActionResult Guncelle(tbl_ogretmen p1)
+        {
+            var musteri = db.tbl_ogretmen.Find(p1.ogretmenid);
+            musteri.ogretmenAd = p1.ogretmenAd;
+            musteri.ogretmenSoyad = p1.ogretmenSoyad;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
     }
 }
